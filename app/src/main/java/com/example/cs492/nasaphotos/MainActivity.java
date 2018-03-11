cab98fdef68f1666c219e5867033938da04b10ab
Francisco <bolanosf@oregonstate.edu>
2018-03-10 16:40:41 -0800

    added class for displaying images on App

--- app/src/main/java/com/example/cs492/nasaphotos/MainActivity.java
+++ app/src/main/java/com/example/cs492/nasaphotos/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.cs492.nasaphotos;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //executes AsyncTask for loading image using url
+        new DownloadImageTask((ImageView) findViewById(R.id.image_1))
+                .execute("https://apod.nasa.gov/apod/image/1803/Cycle-Panel-1200px.jpg");
+    }
+
+
+    //Picture AsyncTask class
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
