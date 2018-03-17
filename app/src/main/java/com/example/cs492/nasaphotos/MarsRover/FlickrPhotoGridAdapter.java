ee7c1f6dd793ab5ec730bfb4a38b6e1446d84731
yangx3 <yxy100good@gmail.com>
2018-03-16 18:08:19 -0700

    trying on Gallery

---
+++ app/src/main/java/com/example/cs492/nasaphotos/MarsRover/FlickrPhotoGridAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cs492.nasaphotos.MarsRover;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.cs492.nasaphotos.R;
+
+/**
+ * Created by 10463 on 3/16/2018.
+ */
+
+public class FlickrPhotoGridAdapter extends RecyclerView.Adapter<FlickrPhotoGridAdapter.FlickrPhotoViewHolder>{
+    private FlickrUtils.FlickrPhoto[] mPhotos;
+
+    public void updatePhotos(FlickrUtils.FlickrPhoto[] photos){
+        mPhotos = photos;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mPhotos != null) {
+            return mPhotos.length;
+        } else {
+            return 0;
+        }
+    }
+
+    class FlickrPhotoViewHolder extends RecyclerView.ViewHolder{
+        private ImageView mPhotoIV;
+
+        public FlickrPhotoViewHolder(View itemView){
+            super(itemView);
+            mPhotoIV =(ImageView)itemView.findViewById(R.id.iv_photo);
+        }
+
+        public void bind(FlickrUtils.FlickrPhoto photo){
+            Glide.with(mPhotoIV.getContext()).load(photo.url_m).into(mPhotoIV);
+        }
+    }
+
+    @Override
+    public FlickrPhotoViewHolder onCreateViewHolder(ViewGroup parent,
+                                                    int viewType) {
+        LayoutInflater inflater =
+                LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.mars_photo_grid_item,
+                parent, false);
+        return new FlickrPhotoViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrPhotoViewHolder holder,
+                                 int position) {
+        holder.bind(mPhotos[position]);
+
+    }
+}
---
+++ app/src/main/java/com/example/cs492/nasaphotos/MarsRover/FlickrUtils.java
@@ -0,0 +1,8 @@
+package com.example.cs492.nasaphotos.MarsRover;
+
+/**
+ * Created by 10463 on 3/16/2018.
+ */
+
+public class FlickrUtils {
+}
