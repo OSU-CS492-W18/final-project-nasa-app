eddfd1cce88bbf83b641d2a8ca5159c804a091b3
yangx3 <yxy100good@gmail.com>
2018-03-18 19:01:26 -0700

    Revert "Fixed empty when video comes"
    This reverts commit c0a3a87722b37be8ff46512a390a4f387f127162.

--- app/src/main/java/com/example/cs492/nasaphotos/utils/ImageofTodayUtil.java
+++ app/src/main/java/com/example/cs492/nasaphotos/utils/ImageofTodayUtil.java
@@ -3,12 +3,9 @@ package com.example.cs492.nasaphotos.utils;
 /**
  * Created by 10463 on 3/11/2018.
  */
-import android.media.MediaMetadataRetriever;
 import android.net.Uri;
-import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
-import android.graphics.Bitmap;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,14 +13,12 @@ import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ImageofTodayUtil {
     final static String DANNY_KEY = "P29hnhHbOt9VpKtIQqFPXisntHkQMdwyj6p0Nb1T";
     final static String IMAGE_OF_TODAY_URL = "https://api.nasa.gov/planetary/apod?api_key=" + DANNY_KEY; //this URL only works for Image of "Today"!
 
     public static class ImageofToday implements Serializable{
-        public String file_type;
         public String date;
         public String explanation;
         public String image_title;
@@ -33,7 +28,6 @@ public class ImageofTodayUtil {
     public static String buildIODSearchURL(){
         return Uri.parse(IMAGE_OF_TODAY_URL).buildUpon().build().toString();
     }
-
 
     //Today
 public static ImageofToday parseIODResultsJSON(String IODResultJSON){
@@ -49,8 +43,6 @@ public static ImageofToday parseIODResultsJSON(String IODResultJSON){
             Log.d("Parse IOD JSON","Read title");
             searchResult.url = searchResultsObj.getString("url");
             Log.d("Parse IOD JSON","Read url");
-            searchResult.file_type = searchResultsObj.getString("media_type");
-            Log.d("Parse IOD JSON","Media type: "+ searchResult.file_type);
             return searchResult;
 
         }catch (JSONException e){
